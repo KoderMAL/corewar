0b2f7cbac3e95574e9e4201d1bfb9fb0d2a2d230
Amine LALAOUI <alalaoui@student.42.fr>
2017-12-01 16:03:37 +0100

    valeurs prises par les instructions live et zjmp en commentaire

---
+++ Resources/try.s
@@ -0,0 +1,16 @@
+.name    "try"
+.comment "I'm trying ASM"
+
+live:
+live %4
+zjmp %:live
+zjmp %:live
+
+# avec live %1
+#01 00 00 00 01 09 ff fb
+# avec live %4
+#01 00 00 00 04 09 ff fb
+# sans le zjmp
+#01 00 00 00 04
+# avec 2 zjmp d'affilee
+#01 00 00 00 04 09 ff fb 09 ff f8
