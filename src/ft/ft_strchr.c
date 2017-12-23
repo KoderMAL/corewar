30fb0d318fbd97d945826dfc6ca97b0fe82f9e07
Stéven Toupin <steven.toupin@gmail.com>
2017-12-23 22:12:05 +0100

    optimized ft_strchr

--- src/ft/ft_strchr.c
+++ src/ft/ft_strchr.c
@@ -12,11 +12,13 @@
 
 #include <stdlib.h>
 
-char		*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != (char)c)
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char*)s);
 		s++;
-	if (*s == (char)c)
-		return ((char*)s);
+	}
 	return (NULL);
 }
