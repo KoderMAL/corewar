8c30d594c55604d29aeed654d8e3b895f17529a5
Amine LALAOUI <alalaoui@student.42.fr>
2018-01-04 18:15:27 +0100

    instructions save start

--- src/parsing/save_instruction.c
+++ src/parsing/save_instruction.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_instruction.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/04 17:48:23 by alalaoui          #+#    #+#             */
+/*   Updated: 2018/01/04 18:14:28 by alalaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "main/asm.h"
 
 /*
@@ -7,7 +19,26 @@
 ** instruction_dup
 */
 
-void	save_instruction(t_env *env)
+static int		check_types(t_instruction *instruction)
 {
-	(void)env;
+return(1);
+}
+
+static int		check_instruction(t_instruction *instruction)
+{
+return (1);	
+}
+
+void			save_instruction(t_env *env)
+{
+	t_instruction *new;
+
+	new = NULL;
+	if (check_instruction(&env->instruction) == -1)
+		err(env, "invalid instruction format\n", 0);
+	else
+	{
+		new = instruction_dup(&env->instruction);
+		pqueue_push(&env->instructions, new);
+	}
 }
