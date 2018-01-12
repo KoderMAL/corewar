9fb95c0229b999871399651ea43d28f877878845
Steven Toupin <steven.toupin@gmail.com>
2018-01-12 17:38:59 +0100

    small improvement in error display

--- src/main/error.c
+++ src/main/error.c
@@ -25,17 +25,17 @@ int		err(t_env *env, char *s, int col_back)
 
 int		err_display(t_env *env)
 {
-	if (env->err)
+	if (env->err && ft_strlen(env->err_msg) > 0)
 	{
 		openfile_write_str(&(env->stderr), "Error: ", 0);
 		openfile_write_str(&(env->stderr), env->err_msg, 1);
-	}
-	if (env->err && env->col >= 0)
-	{
-		openfile_write_str(&(env->stderr), "at line ", 0);
-		openfile_write_nbr(&(env->stderr), env->line, 0);
-		openfile_write_str(&(env->stderr), ", column ", 0);
-		openfile_write_nbr(&(env->stderr), env->col, 1);
+		if (env->col >= 0)
+		{
+			openfile_write_str(&(env->stderr), "at line ", 0);
+			openfile_write_nbr(&(env->stderr), env->line, 0);
+			openfile_write_str(&(env->stderr), ", column ", 0);
+			openfile_write_nbr(&(env->stderr), env->col, 1);
+		}
 	}
 	return (env->err);
 }
