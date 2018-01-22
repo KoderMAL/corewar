a72a4ef5ff5f96fbb3969c676b6c0818bb2f76e5
Daniel Hadley <hadleydaniel@gmail.com>
2018-01-22 15:47:15 +0100

    add debugging info

--- src/assembly/store_locations.c
+++ src/assembly/store_locations.c
@@ -6,7 +6,7 @@
 /*   By: dhadley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 17:45:35 by dhadley           #+#    #+#             */
-/*   Updated: 2018/01/19 17:59:07 by dhadley          ###   ########.fr       */
+/*   Updated: 2018/01/22 15:18:12 by dhadley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void	store_gap(int *LC, t_pqueue *gaps, t_argument arg, int has_idx, int gapLC)
 	}
 	gap->name = arg.name;
 	gap->label = arg.label;
+	printf("label name = %s and gap name = %s and gap->locatins = %d \n", arg.label->name, gap->name, gap->location);
 	if (pqueue_push(gaps, gap))
 		;
 		//return error;
@@ -39,6 +40,6 @@ void	store_gap(int *LC, t_pqueue *gaps, t_argument arg, int has_idx, int gapLC)
 
 void	store_label(int LC, t_label *label)
 {
-	printf("label name = %s LC = %d\n", label->name, LC);
 	label->location = LC;
+	printf("label name = %s LC = %d\n", label->name, label->location);
 }
