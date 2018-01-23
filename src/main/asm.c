d5300c8e080caa43b91c7235305ae413b006be2d
Steven Toupin <steven.toupin@gmail.com>
2018-01-23 17:58:46 +0100

    fixed error override

--- src/main/asm.c
+++ src/main/asm.c
@@ -6,7 +6,7 @@
 /*   By: stoupin <stoupin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 15:49:30 by alalaoui          #+#    #+#             */
-/*   Updated: 2018/01/23 17:55:44 by stoupin          ###   ########.fr       */
+/*   Updated: 2018/01/23 17:58:17 by stoupin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ static void		parse(t_env *env)
 		err(env, "no instruction!", 0);
 	if (env->err == 0)
 		check_labels(env);
-	if (!env->name_check || !env->comment_check)
+	if (env->err == 0 && (!env->name_check || !env->comment_check))
 		err(env, "name or comment not found", -1);
 }
 
