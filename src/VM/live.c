6e884a5b33c4fa0f692316359b4010db6b8902fc
Lara Ramirez <ramirez_li@hotmail.co.uk>
2018-01-25 17:55:30 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/VM/live.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   live.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lramirez <lramirez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/25 11:32:35 by lramirez          #+#    #+#             */
+/*   Updated: 2018/01/25 17:55:05 by lramirez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "op.h"
+#include "vm.h"
+
+void		live(t_vm *vm, t_thread process)
+{
+	char	move;
+	int		id;
+
+	move = 4;
+	while (move)
+	{
+		if (process->location == MEM_SIZE - 1)
+			process->location = 0;
+		(process->location)++;
+		move--;
+	}
+	id = map[process->location] << 24 | map[process->location + 1] << 16 | map[process->location + 2] << 8 | map[process->location + 3];
+	player = 0;
+	while (player < vm->nbr_chmp)
+	{
+		if (id == vm->champs_fd[player]->id)
+		{
+			ft_printf("un processus dit que le joueur %d(%s) est en vie\n", player + 1, vm->champs_fd[player]->name);
+			break;
+		}
+		player++;
+	}
+	move = 4;
+	while (move)
+	{
+		if (process->location == MEM_SIZE - 1)
+			process->location = 0;
+		(process->location)++;
+		move--;
+	}
+	process->cycles -= 10;
+}
