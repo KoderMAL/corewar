e48da1cf956415d558a7bec464da15d6bdf5b554
Unknown <amine_lalaoui@hotmail.com>
2018-01-25 10:48:39 +0100

    Auto stash before merge of "master" and "origin/master"

--- Resources/try.s
+++ Resources/try.s
@@ -3,13 +3,5 @@
 
 live:
 live %4
+add r1, %2, %3
 zjmp %:live
-
-# avec live %1
-#01 00 00 00 01 09 ff fb
-# avec live %4
-#01 00 00 00 04 09 ff fb
-# sans le zjmp
-#01 00 00 00 04
-# avec 2 zjmp d'affilee
-#01 00 00 00 04 09 ff fb 09 ff f8
