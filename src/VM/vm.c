2d4f9e1c9f5c164eba69db89d919c54f21e27c72
Unknown <amine_lalaoui@hotmail.com>
2018-01-26 11:49:04 +0100

    map fill 4 players ok

--- src/VM/vm.c
+++ src/VM/vm.c
@@ -6,7 +6,7 @@
 /*   By: alalaoui <alalaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 14:30:57 by alalaoui          #+#    #+#             */
-/*   Updated: 2018/01/26 11:03:47 by alalaoui         ###   ########.fr       */
+/*   Updated: 2018/01/26 11:48:13 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,11 @@ int main(int ac, char **av)
 
 	i = 0;
 	vm.err = 0;
-	if (ac > MAX_ARGS_NUMBER)
+	if (ac - 1 > MAX_ARGS_NUMBER)
 		err2(&vm, "Too many arguments");
 	else if (ac < 2)
 		err2(&vm, "Usage: ./corewar [champ.cor] [...] (4 files maximum)");
+	printf("ac:%d-MAX%d\n", ac, MAX_ARGS_NUMBER);
 	if (vm.err == 0)
 	{
 		vm_initialization(&vm, ac);
