0fdb9c1d965519c912c3b35c163e7ab22c0d67dd
Daniel Hadley <hadleydaniel@gmail.com>
2018-01-26 18:18:17 +0100

    normally working ld

--- src/VM/ld.c
+++ src/VM/ld.c
@@ -6,7 +6,7 @@
 /*   By: dhadley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 16:50:31 by dhadley           #+#    #+#             */
-/*   Updated: 2018/01/26 15:10:38 by dhadley          ###   ########.fr       */
+/*   Updated: 2018/01/26 18:18:00 by dhadley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,33 @@ Elle load la valeur du premier paramètre dans le registre. Cette opération m
 ld 34,r3 charge les REG_SIZE octets à partir de l’adresse (PC + (34 % IDX_MOD)) dans le registre r3.
 */
 
-static int	check_first_param(unsigned char byte)
-{
-	if ((byte >> 6) & DIR_CODE)
-		return (DIR_CODE);
-	else if ((byte >> 6) & IND_CODE)
-		return (IND_CODE);
-	else if (REG_CODE
-			ret (REG_CODE)
-	return (0);
-}
-
 int	op_ld(t_vm *vm, t_thread *PC)
 {
-	//assuming the PC->location should be on the byte after the opcode
-	if (check_param_byte(vm->map[PC->location]))
+	int	param1;
+	int tmp_ind;
+	int param2;
+
+	//assuming the PC->location should be on the opcode
+	if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 1) == DIR_CODE)
 	{
-
-		PC->location++;
-
+		param1 = recup_param(vm, (PC->location + 2) % MEM_SIZE, 4);
+		param2 = recup_param(vm, (PC->location + 2 + 4) % MEM_SIZE, 1);
+		if (param2 > REG_NUMBER || param2 < 1)
+			param2 = 0;
+		PC->r[param2] = param1;
+		PC->location = (PC->location + 1 + 1 + 4 + 1) % MEM_SIZE;
+		return (1);
 	}
+	else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 1) == IND_CODE)
+	{
+		tmp_ind = recup_param(vm, (PC->location + 2) % MEM_SIZE, 2);
+		param1 = recup_param(vm, (PC->location + tmp_ind) % MEM_SIZE, REG_SIZE);
+		param2 = recup_param(vm, (PC->location + 2 + 2) % MEM_SIZE, 1);
+		if (param2 > REG_MUMBER || param2 < 1)
+			param2 = 0;
+		PC->r[param2] = param1;
+		PC->location = (PC->location + 1 + 1 + 2 + 1) % MEM_SIZE;
+		return (1);
+	}
+	return (0)
 }
