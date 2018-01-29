a14ab08aa6b130e3789c4d1743693465e42cd5e7
Daniel Hadley <hadleydaniel@gmail.com>
2018-01-29 14:49:56 +0100

    ldi finished

--- src/corewar/ldi.c
+++ src/corewar/ldi.c
@@ -6,7 +6,7 @@
 /*   By: dhadley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 11:22:42 by dhadley           #+#    #+#             */
-/*   Updated: 2018/01/29 14:24:14 by dhadley          ###   ########.fr       */
+/*   Updated: 2018/01/29 14:49:46 by dhadley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,8 @@ On lit REG_SIZE octet à l’adresse (PC + (S % IDX_MOD)) que l’on copie dans
 Les paramètres 1 et 2 sont des index.
 */
 
-/*
-p1 = T_REG | T_DIR | T_IND
-p2 = T_DIR | T_REG
-p3 = T_REG
-
-dir is on 2
-*/
-
-/*
-if p1 = T_REG, check if valid reg on 1 oct. get value. store as p1.
-
-
-if p1 = T_DIR, read value on 2 bytes. p1 = that value;
-
-if p1 = T_IND, read two bytes. Go fetch the value IND_SIZE at the address (PC + (tmp_ind % IDXMOD)). p1 = that value
-
-if p2 =T_DIR, read 2 bytes. p2 = that value. sum =p1 + p2;
-if p2 = T_REG, read 1 byte. check if valid reg. get value. sum = p1 + p2;
-
-read REG_SIZE bytes at the address (PC + (sum % IDX_MOD)) 
-
-if p3 = T_REG, read 1 byte. check if valid reg. get value. r[x] = sum
-*/
-
+//need to take off cycles
+//need to modify carry
 
 int	op_ldi(t_vm *vm, t_thread *PC)
 {
@@ -67,8 +45,11 @@ int	op_ldi(t_vm *vm, t_thread *PC)
 				if (param1 > REG_NUMBER || param1 < 1)
 					return (0);
 				PC->r[param1] = tmp;
-				//			01	37	00 03	00 05	06
+				PC->location = (PC->location + 1 + 3 + 2 + 1) % MEM_SIZE;
+				return (1);
 			}
+			else
+				return (0);
 		}
 		else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 2) == REG_CODE)
 		{
@@ -77,6 +58,105 @@ int	op_ldi(t_vm *vm, t_thread *PC)
 				return (0);
 			param2 = PC->r[tmp];
 			sum = param1 + param2;
+			tmp = recup_param(vm, (PC->location + (sum % IDX_MOD)) % MEM_SIZE, REG_SIZE);
+			if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 3) == REG_CODE)
+			{
+				param1 = recup_param(vm, (PC->location + 1 + 3 + 1) % MEM_SIZE, 1);
+				if (param1 > REG_NUMBER || param1 < 1)
+					return (0);
+				PC->r[param1] = tmp;
+				PC->location = (PC->location + 1 + 3 + 1 + 1) % MEM_SIZE;
+				return (1);
+			}
+			else
+				return (0);
 		}
 	}
+	else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 1) == DIR_CODE)
+	{
+		param1 = recup_param(vm, (PC->location + 2) % MEM_SIZE, IND_SIZE);
+		if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 2) == DIR_CODE)
+		{
+			param2 = recup_param(vm, (PC->location + 1 + 3) % MEM_SIZE, IND_SIZE);
+			sum = param1 + param2;
+			tmp = recup_param(vm, (PC->location + (sum % IDX_MOD)) % MEM_SIZE, REG_SIZE);
+			if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 3) == REG_CODE)
+			{
+				param1 = recup_param(vm, (PC->location + 1 + 3 + 2) % MEM_SIZE, 1);
+				if (param1 > REG_NUMBER || param1 < 1)
+					return (0);
+				PC->r[param1] = tmp;
+				PC->location = (PC->location + 1 + 3 + 2 + 1) % MEM_SIZE;
+				return (1);
+			}
+			else
+				return (0);
+		}
+		else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 2) == REG_CODE)
+		{
+			tmp = recup_param(vm, (PC->location + 1 + 3) % MEM_SIZE, 1);
+			if (tmp > REG_NUMBER || tmp < 1)
+				return (0);
+			param2 = PC->r[tmp];
+			sum = param1 + param2;
+			tmp = recup_param(vm, (PC->location + (sum % IDX_MOD)) % MEM_SIZE, REG_SIZE);
+			if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 3) == REG_CODE)
+			{
+				param1 = recup_param(vm, (PC->location + 1 + 3 + 1) % MEM_SIZE, 1);
+				if (param1 > REG_NUMBER || param1 < 1)
+					return (0);
+				PC->r[param1] = tmp;
+				PC->location = (PC->location + 1 + 3 + 1 + 1) % MEM_SIZE;
+				return (1);
+			}
+			else
+				return (0);
+		}
+	}
+	else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 1) == REG_CODE)
+	{
+		tmp = recup_param(vm, (PC->location + 2) % MEM_SIZE, 1);
+		if (tmp > REG_NUMBER || tmp < 1)
+			return (0);
+		param1 = PC->r[tmp];
+		if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 2) == DIR_CODE)
+		{
+			param2 = recup_param(vm, (PC->location + 1 + 2) % MEM_SIZE, IND_SIZE);
+			sum = param1 + param2;
+			tmp = recup_param(vm, (PC->location + (sum % IDX_MOD)) % MEM_SIZE, REG_SIZE);
+			if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 3) == REG_CODE)
+			{
+				param1 = recup_param(vm, (PC->location + 1 + 2 + 2) % MEM_SIZE, 1);
+				if (param1 > REG_NUMBER || param1 < 1)
+					return (0);
+				PC->r[param1] = tmp;
+				PC->location = (PC->location + 1 + 2 + 2 + 1) % MEM_SIZE;
+				return (1);
+			}
+			else
+				return (0);
+		}
+		else if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 2) == REG_CODE)
+		{
+			tmp = recup_param(vm, (PC->location + 1 + 2) % MEM_SIZE, 1);
+			if (tmp > REG_NUMBER || tmp < 1)
+				return (0);
+			param2 = PC->r[tmp];
+			sum = param1 + param2;
+			tmp = recup_param(vm, (PC->location + (sum % IDX_MOD)) % MEM_SIZE, REG_SIZE);
+			if (check_params(vm->map[(PC->location + 1) % MEM_SIZE], 3) == REG_CODE)
+			{
+				param1 = recup_param(vm, (PC->location + 1 + 2 + 1) % MEM_SIZE, 1);
+				if (param1 > REG_NUMBER || param1 < 1)
+					return (0);
+				PC->r[param1] = tmp;
+				PC->location = (PC->location + 1 + 2 + 1 + 1) % MEM_SIZE;
+				return (1);
+			}
+			else
+				return (0);
+		}
+	}
+	else
+		return (0);
 }
