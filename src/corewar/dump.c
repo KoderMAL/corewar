17e464aef0bab249bd453a429d0e7b4aafe20bcc
Daniel Hadley <hadleydaniel@gmail.com>
2018-01-31 12:12:52 +0100

    add dump in hexa

--- src/corewar/dump.c
+++ src/corewar/dump.c
@@ -6,12 +6,38 @@
 /*   By: alalaoui <alalaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 12:00:20 by alalaoui          #+#    #+#             */
-/*   Updated: 2018/01/30 17:55:36 by alalaoui         ###   ########.fr       */
+/*   Updated: 2018/01/31 12:11:09 by dhadley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft/ft.h"
 #include "vm.h"
+
+static void	write_map(t_vm *vm)
+{
+	char				converted[MEM_SIZE * 3 + MEM_SIZE / 32 + 1];
+	unsigned char		byte;
+	static const char	digits[16] = "0123456789ABCDEF";
+	int					i;
+	int					j;
+
+	i = 0;
+	j = 0;
+	while (i < MEM_SIZE * 3)
+	{
+		byte = vm->map[i];
+		converted[j] = digits[byte / 16];
+		converted[j + 1] = digit[byte % 16]
+		j += 2;
+		if (j % 64 == 0)
+			converted[j] = '\n';
+		else
+			converted[j] = ' ';
+		j++;
+		i++;
+	}
+	write(1, converted, MEM_SIZE * 3);
+}
 
 void		dump(t_vm *vm)
 {
@@ -22,13 +48,8 @@ void		dump(t_vm *vm)
 	{
 		if (vm->game_cycle == vm->cycle_to_dump)
 		{
-			while (i < MEM_SIZE)
-			{
-				printf("%x ", vm->map[i]);
-				i++;
-				if (i % 64 == 0)
-					{printf("\n");}
-			}
+			write_map(vm);
+		}
 			exit(1);
 		}
 	}
