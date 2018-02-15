bd9f3cd55bf0bcc538bc781931b9b0ce6531c883
Stéven Toupin <steven.toupin@gmail.com>
2018-02-15 12:35:13 +0100

    verbose zjmp

--- src/corewar/zjmp.c
+++ src/corewar/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: stoupin <stoupin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 17:58:09 by lramirez          #+#    #+#             */
-/*   Updated: 2018/02/08 16:48:55 by stoupin          ###   ########.fr       */
+/*   Updated: 2018/02/15 12:29:51 by stoupin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,22 @@ int	op_zjmp(t_vm *vm, t_thread *process)
 {
 	short	index;
 
+	print_instruction_start(vm, process);
+	print_str(vm, "zjmp ", 0);
 	process->location = (process->location + 1) % MEM_SIZE;
 	index = vm->map[process->location] << 8
 			| vm->map[(process->location + 1) % MEM_SIZE];
+	print_nbr(vm, index, 0);
 	if (process->carry == 1)
+	{
 		process->location = (process->location + (index % IDX_MOD)) % MEM_SIZE;
+		print_str(vm, " OK", 1);
+	}
 	else
+	{
 		process->location = (process->location + 2) % MEM_SIZE;
+		print_str(vm, " FAILED", 1);
+	}
 	process->cycles -= 20;
 	return (1);
 }
