5adf97807f15d02584ab97b0d568d3c38fc881c0
Daniel HADLEY <hadleyd@tcd.ie>
2018-02-21 19:02:55 +0100

    new champs for testing

--- mars_attack/piscine.s
+++ mars_attack/piscine.s
@@ -1,13 +1,24 @@
 .name "Splish-Splosh"
 .comment "Dan's first try at making a champion."
-
-		lld %0, r7
-	#	ld %1, r6
-	#	lld 10, r7
-		zjmp %-120
+		
+# FORK block which zjmps on live and fork
+		ld %0, r7 #to make first zjmp work
+		ld %15, r4
+		fork %:block2
 		st r1, 12
+		st r1, 45 #modify + to get to the :live1
 start:	fork %:heart
-	#	zjmp %:start
+		zjmp %:start
 heart: 	live %42
+		ld %0, r7 #to make the second zjmp work with the pc from start/fork
+		zjmp %:heart
+# end for FORK block
+
+block2:  ld %9999, r2
+
+loop:	add r3,r4,r3
+		sti r2,r3,%11
+live1:	live %42
 		ld %0, r7
-	#	zjmp %:start
+		zjmp %:loop
+
