96f61224d56c94233af4d6662292168fbec2ffd8
Daniel HADLEY <hadleyd@tcd.ie>
2018-02-22 18:33:59 +0100

    work on champ

--- mars_attack/piscine.s
+++ mars_attack/piscine.s
@@ -5,9 +5,13 @@
 		ld %0, r7 #to make first zjmp work
 		ld %15, r4
 		fork %:block2
-		st r1, 12
-		st r1, 45 #modify + to get to the :live1
+		live %999999
+		st r1, 34 #modify to the heart live
+		st r1, 62 #modify + to get to the :live1
+		st r1, 9 #modify + to get to the :start live
 start:	fork %:heart
+		live %42
+		ld %0, r7
 		zjmp %:start
 heart: 	live %42
 		ld %0, r7 #to make the second zjmp work with the pc from start/fork
@@ -15,10 +19,11 @@ heart: 	live %42
 # end for FORK block
 
 block2:  ld %9999, r2
-
 loop:	add r3,r4,r3
 		sti r2,r3,%11
 live1:	live %42
 		ld %0, r7
 		zjmp %:loop
+
+
 
