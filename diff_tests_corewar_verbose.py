ff605b7789bf972a8b1200dcc89fc8d16e07f776
Stéven Toupin <steven.toupin@gmail.com>
2018-02-22 14:26:19 +0100

    fixed diff test verbose

--- diff_tests_corewar_verbose.py
+++ diff_tests_corewar_verbose.py
@@ -43,21 +43,21 @@ for champ in champs:
 	error, _, champ = compile_champ(ZAZ_ASM, champ)
 	if error:
 		continue
-	for i in range(0, 64):
-		zaz_error, zaz_output = get_dump([ZAZ_CW, '-v 20', champ, champ])
-		my_error, my_output = get_dump([MY_CW, '-verbose', champ, champ])
-		if (zaz_error and not my_error) or (my_error and not zaz_error):
-			if zaz_error:
-				print zaz_output
-				sys.exit(1)
-			if my_error:
-				print my_output
-				sys.exit(1)
-		if not my_error and zaz_output != my_output:
-			print('dump number {}'.format(i))
-			with open('/tmp/zaz', 'w') as f:
-				f.write(zaz_output)
-			with open('/tmp/me', 'w') as f:
-				f.write(my_output)
-			print(subprocess.check_output(['/usr/bin/diff', '/tmp/zaz', '/tmp/me']).decode('utf-8'))
+	zaz_error, zaz_output = get_dump([ZAZ_CW, '-v 20', champ, champ])
+	my_error, my_output = get_dump([MY_CW, '-verbose', champ, champ])
+	if (zaz_error and not my_error) or (my_error and not zaz_error):
+		if zaz_error:
+			print('ZAZ OUTPUT')
+			print(zaz_output)
 			sys.exit(1)
+		if my_error:
+			print('OUR OUTPUT')
+			print(my_output)
+			sys.exit(1)
+	if not my_error and zaz_output != my_output:
+		with open('/tmp/zaz', 'w') as f:
+			f.write(zaz_output)
+		with open('/tmp/me', 'w') as f:
+			f.write(my_output)
+		print(subprocess.check_output(['/usr/bin/diff', '/tmp/zaz', '/tmp/me']).decode('utf-8'))
+		sys.exit(1)
