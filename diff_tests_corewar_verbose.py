bcfc98c87d781853ea5e864c584997c6bd69b5b2
Lara Ramirez <ramirez_li@hotmail.co.uk>
2018-03-03 19:01:13 +0100

    Nouveau script multithread (sweeeet)

--- diff_tests_corewar_verbose.py
+++ diff_tests_corewar_verbose.py
@@ -1,4 +1,5 @@
-import os, shutil, subprocess, sys
+import os, shutil, subprocess, sys, shlex
+
 
 MY_CW = './corewar'
 ZAZ_CW = './resources/corewar'
@@ -29,6 +30,19 @@ def compile_champ(asm, champ):
 		result_data = f.read()
 	return False, bytearray(result_data), result_file_name
 
+def run_command(command_1, command_2):
+	print command_1
+	print command_2
+	process_1 = subprocess.Popen(shlex.split(command_1), stdout=subprocess.PIPE)
+	process_2 = subprocess.Popen(shlex.split(command_2), stdout=subprocess.PIPE)
+	while True:
+		output_1 = process_1.stdout.readline()
+		output_2 = process_2.stdout.readline()
+		if output_1 != output_2:
+			print output_1.ljust(50) + output_2
+		if output_1 == '' and process_1.poll() is not None and output_2 == '' and process_2.poll() is not None:
+			break
+
 champs = list_champs()
 iterator = iter(champs)
 name_champ2 = next(iterator)
@@ -38,15 +52,16 @@ for champ in iterator:
 	print('{} vs. {}'.format(name_champ1, name_champ2))
 	error, _, champ1 = compile_champ(ZAZ_ASM, name_champ1)
 	if error:
-		continue
+			continue
 	
 	error, _, champ2 = compile_champ(ZAZ_ASM, name_champ2)
 	if error:
-		continue
-	os.system('./resources/corewar -v 31 {} {} > {}/goinfre/zaz'.format(champ1, champ2, HOME_DIR))
-	os.system('./corewar -zaz -verbose {} {} > {}/goinfre/me'.format(champ1, champ2, HOME_DIR))
-	zaz_output = open('{}/goinfre/zaz'.format(HOME_DIR), 'r').read()
-	my_output = open('{}/goinfre/me'.format(HOME_DIR), 'r').read()
-	if zaz_output != my_output:
-		os.system('/usr/bin/vimdiff {}/goinfre/zaz {}/goinfre/me'.format(HOME_DIR, HOME_DIR))
-		sys.exit(1)
+			continue
+	run_command('./resources/corewar -v 31 {} {}'.format(champ1, champ2),
+	'./corewar -zaz -verbose {} {}'.format(champ1, champ2))
+	print('')
+	# zaz_output = open('{}/goinfre/zaz'.format(HOME_DIR), 'r').read()
+	# my_output = open('{}/goinfre/me'.format(HOME_DIR), 'r').read()
+	# if zaz_output != my_output:
+	# 		# os.system('/usr/bin/vimdiff {}/goinfre/zaz {}/goinfre/me'.format(HOME_DIR, HOME_DIR))
+	# 	sys.exit(1)
